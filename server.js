e50680ff005722cbbec21297c69fe633d77ecb0a
Manuel-Pi <psykofou@hotmail.com>
2015-11-16 15:02:57 +0100

    websocket

--- server.js
+++ server.js
@@ -174,16 +174,33 @@ var server = http.createServer(app);
 // Chargement de socket.io
 var io = require('socket.io').listen(server);
 
+var users = [];
+
 // Quand on client se connecte, on le note dans la console
 io.sockets.on('connection', function (socket) {
-    console.log('Un client est connecté !');
-    socket.emit('message', 'Vous êtes bien connecté !');
+    
+    socket.on('login', function (pseudo) {
+        socket.emit('message', {text: 'Connected!', user: "server"});
+        console.log('Connection: ' + pseudo);
+        socket.user = pseudo;
+        users.push(pseudo);
+        socket.emit('users', users);
+        socket.broadcast.emit('users', users);
+    });
     
      // Quand le serveur reçoit un signal de type "message" du client
     socket.on('message', function (message) {
-        console.log('Un client me parle ! Il me dit : ' + message);
         socket.broadcast.emit('message', message);
-    }); 
+    });
+    
+    socket.on('disconnect', function () {
+        console.log('Deconnexion: ');
+        var i = users.indexOf(socket.user);
+        if(i > -1){
+            delete users[i];
+        }
+        socket.emit('users', users);
+    });
 });
 
 
