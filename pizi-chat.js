8fd810d92f708e60adb04fb94d20e006896140e4
Manuel_Pi <psykofou@hotmail.com>
2015-12-31 13:26:03 +0100

    ...

--- pizi-chat.js
+++ pizi-chat.js
@@ -42,6 +42,7 @@ module.exports = function(server){
         socket.on('disconnect', function () {
             var i = users.indexOf(socket.user);
             if(i > -1){
+                deleteUserFromRooms(users[i]);
                 console.log('Deconnexion: ' + users[i])
                 users.splice(i, 1);
             }
@@ -53,24 +54,34 @@ module.exports = function(server){
                 if(room.id === roomId){
                     if(room.authorized === "All" || room.authorized.indexOf(socket.user) > -1){
                         socket.join(roomId);
+                        socket.emit('roomData', room);
                         room.connected.push(socket.user);
                         socket.broadcast.to(roomId).emit('userJoinRoom', {roomId: roomId, user: socket.user});
                         console.log(socket.user + " join room: " + roomId);
+                        console.log(room);
                     }
                     break;
                 }
             }
         });
-        
-        socket.on('leaveRoom', function (roomId) {
-             for(var room of rooms){
-                if(room.id === roomId){
-                    socket.leave(roomId);
-                    room.connected.slice(room.connected.indexOf(socket.user), 1);
-                    socket.broadcast.to(roomId).emit('userLeaveRoom', {roomId: roomId, user: socket.user});
-                    console.log(socket.user + " leave room: " + roomId);
+       
+        var deleteUserFromRooms = function(user, roomId){
+            var i = rooms.length;
+            while(i--){
+                var room = rooms[i];
+                if((room.authorized === "All" || room.authorized.indexOf(user) > -1) && (!roomId || roomId === room.id)){
+                   room.connected.splice(room.connected.indexOf(user), 1);
+                   socket.broadcast.to(room.id).emit('userLeaveRoom', {roomId: room.id, user: user});
+                   console.log(user + " leave room: " + room.id);
+                   console.log(room);
+                   if(roomId) break;
                 }
              }
+        };
+        
+        socket.on('leaveRoom', function (roomId) {
+            socket.leave(roomId);
+            deleteUserFromRooms(socket.user, roomId);
         });
         
         socket.on('message', function (message) {
