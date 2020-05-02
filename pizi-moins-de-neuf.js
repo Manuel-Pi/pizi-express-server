d2391f8bf6f944986823add073f7dcb96c066702
Pizi <manu@MacBook-Pro-de-Manu.local>
2020-05-02 13:58:38 -0400

    asdasda

--- pizi-moins-de-neuf.js
+++ pizi-moins-de-neuf.js
@@ -230,6 +230,7 @@ module.exports = function(server){
                     }
                 });
             });
+            socket.emit('setGames', getPublicGames(GAMES));
          });
 
         socket.on('quit', () => {
